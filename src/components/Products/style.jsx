58f5985d8a3aa9ec239584c13016342fc5f355fa
Valente <108316067+ValenteJoao@users.noreply.github.com>
2023-09-12 15:44:20 -0300

    Alteração fontes

--- src/components/Products/style.jsx
+++ src/components/Products/style.jsx
@@ -1,78 +1,78 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-display: flex;
-flex-direction: column;
-align-items: center;
-align-content: center;
-justify-content: space-between;
-background-color: #ffffff;
-width:280px;
-height:60vh;
-padding-top: 10px;
-border-radius:8px;
-box-shadow: 0px 2px 8px rgba(0, 0, 0, 0.135216);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  align-content: center;
+  justify-content: space-between;
+  background-color: #ffffff;
+  width: 280px;
+  height: 60vh;
+  padding-top: 10px;
+  border-radius: 8px;
+  box-shadow: 0px 2px 8px rgba(0, 0, 0, 0.135216);
 
-img{
-  max-width: 120px;
-}
-`
+  img {
+    max-width: 120px;
+  }
+`;
 export const Description = styled.span`
-font-family: Montserrat;
-text-align: center;
+  font-family: Arial, Helvetica, sans-serif;
+  text-align: center;
   font-size: 0.6em;
   font-weight: 300;
-  color: #2C2C2C;
-  `
+  color: #2c2c2c;
+`;
 
 export const ContainerInfo = styled.div`
-display: flex;
-flex-direction: column;
-justify-content: center;
-width: 90%;
-gap: 3px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 90%;
+  gap: 3px;
 
-strong{
-  text-align: center;
-  font-size: 0.9em;
-  font-weight: 500;
-  font-family: Montserrat;
-  color: #2C2C2C;
-}
-`
+  strong {
+    text-align: center;
+    font-size: 0.9em;
+    font-weight: 500;
+    font-family: Arial, Helvetica, sans-serif;
+    color: #2c2c2c;
+  }
+`;
 export const ContainerPrice = styled.div`
-background-color:#373737;
-padding: 5px;
-border-radius: 6px;
-margin: 0 0 6px 5px;
+  background-color: #373737;
+  padding: 5px;
+  border-radius: 6px;
+  margin: 0 0 6px 5px;
 
-span{
-  font-size: 0.9em;
-  font-weight: 700;
-  color: #ffffff;
-}
-`
+  span {
+    font-size: 0.9em;
+    font-weight: 700;
+    color: #ffffff;
+  }
+`;
 
 export const BtnContainer = styled.button`
-display: flex;
-align-items: center;
-justify-content: center;
-background-color: #0F52BA;
-width: 100%;
-height: 25px;
-border-radius:0 0 8px 8px;
-border: none;
-cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #0f52ba;
+  width: 100%;
+  height: 25px;
+  border-radius: 0 0 8px 8px;
+  border: none;
+  cursor: pointer;
 
-span{
-font-size:14px;
-font-family: Montserrat;
-font-weight: 600;
-margin-left: 8px;
-color: #ffffff;
-}
+  span {
+    font-size: 14px;
+    font-family: Arial, Helvetica, sans-serif;
+    font-weight: 600;
+    margin-left: 8px;
+    color: #ffffff;
+  }
 
-img{
-  max-width: 12px;
-}
-`
+  img {
+    max-width: 12px;
+  }
+`;
--- src/layout/Header/style.jsx
+++ src/layout/Header/style.jsx
@@ -1,62 +1,61 @@
 import styled from "styled-components";
 
 export const Container = styled.header`
-display: flex;
-height: 60px;
-width: 100vw;
-margin: 0;
-background-color:#0F52BA ;
-align-items: center;
-justify-content: space-around;
-gap: 20%;
+  display: flex;
+  height: 60px;
+  width: 100vw;
+  margin: 0;
+  background-color: #0f52ba;
+  align-items: center;
+  justify-content: space-around;
+  gap: 20%;
 `;
 
 export const Logo = styled.div`
-display: flex;
-height: 30px;
-width: 200px;
-display: flex;
-align-items: flex-end;
-justify-content: center;
+  display: flex;
+  height: 30px;
+  width: 200px;
+  display: flex;
+  align-items: flex-end;
+  justify-content: center;
 
+  h1 {
+    font-size: 27px;
+    font-family: Arial, Helvetica, sans-serif;
+    font-weight: 700;
+    color: white;
+  }
 
-h1{
-  font-size:27px;
-  font-family: montserrat;
-  font-weight: 700;
-  color: white;
-}
-
-.logodetail{
-  text-align: end;
-  font-size:16px;
-  font-weight: 300;
-  margin:0 5px 4px
-}
-`
+  .logodetail {
+    text-align: end;
+    font-size: 16px;
+    font-weight: 300;
+    margin: 0 5px 4px;
+  }
+`;
 
 export const ContainerBtn = styled.button`
-display:flex;
-align-items: center;
-justify-content: space-evenly;
-width: 5em;
-height: 2.5em;
-background-color: #ffffff;
-border: none;
-padding: 5px;
-border-radius:8px;
-cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  width: 5em;
+  height: 2.5em;
+  background-color: #ffffff;
+  border: none;
+  padding: 5px;
+  border-radius: 8px;
+  cursor: pointer;
 
-img{
-  max-width: 20px;
-padding-top:6px;
-}
+  img {
+    max-width: 20px;
+    padding-top: 6px;
+  }
 
-span{
-font-family: Montserrat;
-font-weight:700;
-font-size: 17px;
-color: black;
-padding-right: 14px;
-}
-`
+  span {
+    font-family: Montserrat;
+    font-weight: 700;
+    font-size: 17px;
+    color: black;
+    padding-right: 14px;
+  }
+`;
